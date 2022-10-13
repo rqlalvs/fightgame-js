30732a8b9e0465739a47f9079518deeb0b650d01
rqlalvs <raquelalves3753@gmail.com>
2022-10-13 12:12:29 -0300

    feat: timer funcional e mensagem de vitoria ou empate

--- index.js
+++ index.js
@@ -125,14 +125,42 @@ const keys = {
 function rectangularColligion({ rectangle1, rectangle2 }) {
   return (
     rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
-      rectangle2.position.x &&
+    rectangle2.position.x &&
     rectangle1.attackBox.position.x <=
-      rectangle2.position.x + rectangle2.width &&
+    rectangle2.position.x + rectangle2.width &&
     rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
-      rectangle2.position.y &&
+    rectangle2.position.y &&
     rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
   );
 }
+
+function determineWinner({ player, enemy, timerId}) {
+  clearTimeout(timerId);
+  document.querySelector('#displayText').style.display = 'Flex';
+  if (player.health === enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Empate';
+  }
+  if (player.health > enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Vitória do Jogador 1';
+  }
+  if (player.health < enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Vitória do Jogador 2';
+  }
+}
+
+let timer = 60;
+let timerId;
+function decreaseTimer() {
+  timerId = setTimeout(decreaseTimer, 1000);
+  if (timer > 0) {
+    timer--;
+    document.querySelector('#timer').innerHTML = timer;
+  }
+  if (timer === 0) {
+    determineWinner({ player, enemy, timerId });
+  }
+}
+decreaseTimer();
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -176,6 +204,11 @@ function animate() {
     enemy.isAttacking = false;
     player.health -= 20;
     document.querySelector("#playerHealth").style.width = player.health + "%";
+  }
+  //game over - death
+
+  if (enemy.health <= 0 || player.health <= 0) {
+    determineWinner({ player, enemy, timerId });
   }
 }
 
